9003d3cc0f7f2e8ced0991965d0fdde1c14c93ba
Chu Thuy Hien <yenchuchu95@gmail.com>
2015-11-26 21:31:48 +0700

    update style, add delete, edit

--- www/js/controllers/menuController.js
+++ www/js/controllers/menuController.js
@@ -66,12 +66,35 @@ app.controller('menuController',modalController);
           };//END CREATE
         } else {
           var alertPopup = $ionicPopup.alert({
-            title: 'Sign up ERROR'
+            title: 'CREATE ERROR'
           });
           return alertPopup;
         }
       }
 
+      // change details
+      $ionicModal.fromTemplateUrl('templates/ChangeDetails.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.ChangeDetails = modal;
+      });
+      $scope.EditFood = function () {
+       $scope.EditFood.show();
+       $rootScope.items.push({'foodName':$("#foodName").val(),'foodPrice':$("#foodPrice").val()});
+     }
+
+      $scope.DeleteFood = function () {
+       $scope.DeleteFood.show();
+     }
+     
+      //Edit food
+      $ionicModal.fromTemplateUrl('templates/EditFood.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.EditFood = modal;
+      });
       // EDIT ITEM
       $scope.EditItem = function (isValid) {
 
@@ -104,12 +127,56 @@ app.controller('menuController',modalController);
           };//END CREATE
         } else {
           var alertPopup = $ionicPopup.alert({
-            title: 'Sign up ERROR'
+            title: 'EDIT ERROR'
           });
           return alertPopup;
         }
       }
+
       // DELETE ITEM
+      $ionicModal.fromTemplateUrl('templates/DeleteFood.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.DeleteFood = modal;
+      });
+
+      $scope.DeleteFood = function (isValid) {
+
+        if (isValid) {
+           $scope.DeleteFood = function(u) {        
+            var FoodItem = Parse.Object.extend("FoodItem");
+            var foodItem= new FoodItem();
+            var user=Parse.User.current();
+                foodItem.set("foodName",$("#foodName").val());
+            var str1= $("#foodPrice").val();
+            foodItem.set("foodPrice", parseInt(str1) );
+            foodItem.set("user",user);
+            foodItem.save(null,{
+              success:function(foodItem){
+                // Execute any logic that should take place after the object is saved.
+               // alert('New object created with objectId: ' +foodItem.id);
+                $rootScope.items.push({'foodName':$("#foodName").val(),'foodPrice':$("#foodPrice").val()});
+                var alertPopup = $ionicPopup.alert({
+                  title: 'Edit Item  SUCCESSFULLY'
+                  });
+              },
+              error:function(foodItem,error){
+                 // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('Failed to create new object, with error code: ' + error.message);
+              }
+
+            });
+            $scope.DeleteFood.hide(); 
+          };//END CREATE
+        } else {
+          var alertPopup = $ionicPopup.alert({
+            title: 'DELETE ERROR'
+          });
+          return alertPopup;
+        }
+      }
 
 }
      // $scope.deleteItem(id){}
