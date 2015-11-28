db8fb0528fbfa2d029903ee6433f88df4d1bbd02
tupm58 <kittkatt.liucuxiu@gmail.com>
2015-11-28 20:40:49 +0700

    fix F5

--- www/js/controllers/orderDetailController.js
+++ www/js/controllers/orderDetailController.js
@@ -3,10 +3,34 @@ app.controller('orderDetailController',OrderDetailController);
 		$scope.tableId=$rootScope.tableId;
 		$rootScope.tableId=$routeParams.tableId;
 		
+		
+		//$scope.items=$rootScope.items;
+		$rootScope.items = [];
 
-		$scope.items=$rootScope.items;
+      var FoodItem=Parse.Object.extend("FoodItem");
+          var query= new Parse.Query(FoodItem);
+          query.find({
+            success:function(results){
+              $scope.$apply(function(){
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    
+                        $rootScope.items.push({'foodName':object.get("foodName"),'foodPrice':object.get("foodPrice")});
+                      
+                }
+              });
+            },
+            error: function(){
+                alert("Error: " + error.code + " " + error.message);
+            }
+          });
 		$scope.query={}
 		$scope.queryBy='$';
 		$scope.orderProp="foodName";
+
+		$scope.addToOrder=function(){
+			$scope.select=[];
+
+		};
 	}
 
