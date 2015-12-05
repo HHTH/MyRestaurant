9dbc402df8fbf652dab7e6430d5ca751dca76125
tupm58 <kittkatt.liucuxiu@gmail.com>
2015-12-05 20:02:07 +0700

    orderController

--- www/js/controllers/orderController.js
+++ www/js/controllers/orderController.js
@@ -9,4 +9,41 @@ function OrderDetailController($routeParams,$ionicPopup,$location,$scope,$rootSc
 	$scope.myOrder7 = JSON.parse(localStorage.getItem("order7"));
 	$scope.myOrder8 = JSON.parse(localStorage.getItem("order8"));
 	$scope.myOrder9 = JSON.parse(localStorage.getItem("order9"));
+	// suppose we have a book object
+// 	$rootScope.orders=[];
+// 	$scope.myCartItems=[];
+// 	var Order = Parse.Object.extend("Order");
+// 	var order=new Order();
+// //var Topping = Parse.Object.extend("Specialty");
+// var MyCartItems = Parse.Object.extend("MyCartItems");
+// var myCartItem=order.relation("myCartItem");
+// var query = new Parse.Query(Order);
+// query.find({
+//   success: function(results) {
+//   	$scope.$apply(function(){
+//     for(var i = 0; i < results.length; i++) {
+//       var order = results[i];
+//       var relation = order.relation('myCartItem');
+//       relation.query().find({
+      	
+//         success: function(myCartItem) {
+//         	$scope.$apply(function(){
+//           // toppings is a list of toppings for this pizza
+// 		          for(var j = 0; j < myCartItem.length; j++) {
+// 		            console.log(myCartItem[j].get("foodName"));
+// 		         	$scope.myCartItems.push({'foodName':myCartItem[j].get("foodName")});
+// 		          }
+//            //   $rootScope.orders.push({'myCartItem':$scope.myCartItems});
+//           });
+//         }
+//       });
+//     $rootScope.orders.push({'cart':order.get("cartSum")});
+
+//     }
+//     });
+//   },
+//   error: function(error) {
+//     alert("Error: " + error.code + " " + error.message);
+//   }
+// });
 }
